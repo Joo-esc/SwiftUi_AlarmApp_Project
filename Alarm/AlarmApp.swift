1cb6f94b66781917a5b80d25fab409f0ba3a4af3
Joo-esc <hj_lee_com@naver.com>
2022-01-06 02:29:23 +0900

    Initial Commit

---
+++ Alarm/AlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmApp.swift
+//  Alarm
+//
+//  Created by 이해주 on 2022/01/06.
+//
+
+import SwiftUI
+
+@main
+struct AlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Alarm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Alarm
+//
+//  Created by 이해주 on 2022/01/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
